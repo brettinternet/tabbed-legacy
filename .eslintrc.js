7936918a4be276b833091026c01d5687b3d2e5d2
brett <brettinternet@gmail.com>
2021-07-09 15:20:15 -0600

    test: fix lint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
+    project: './tsconfig.json',
     extraFileExtensions: ['.svelte'],
   },
   plugins: [
@@ -17,12 +22,28 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
-    'eslint-plugin-jest',
+    'plugin:jest/recommended',
   ],
   overrides: [
     {
       files: ['*.svelte'],
       processor: 'svelte3/svelte3',
+    },
+    {
+      files: ['*.js'],
+      env: {
+        node: true,
+      },
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+      },
+    },
+    {
+      files: ['bin/**/*.ts'],
+      env: {
+        node: true,
+      },
     },
   ],
   settings: {
--- bin/clean.ts
+++ bin/clean.ts
@@ -2,7 +2,7 @@ import { rmSync } from 'fs'
 import { resolve } from 'path'
 
 const projectRoot = resolve(__dirname, '..')
-const queue = [resolve(projectRoot, '.dist')]
+const queue = [resolve(projectRoot, '.dist'), resolve(projectRoot, 'coverage')]
 
 queue.forEach(dir => {
   rmSync(dir, { recursive: true })
