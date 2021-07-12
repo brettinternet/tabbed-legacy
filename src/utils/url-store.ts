b703506f9f0ddeafddad50517967c3cdc834a9a7
brett <brettinternet@gmail.com>
2021-07-11 20:53:33 -0600

    feat: url store

---
+++ src/utils/url-store.ts
@@ -0,0 +1,23 @@
+import { derived, writable } from 'svelte/store'
+
+const href = writable(window.location.href)
+
+const originalPushState = history.pushState
+const originalReplaceState = history.replaceState
+
+const updateHref = () => href.set(window.location.href)
+
+history.pushState = function() {
+  originalPushState.apply(this, arguments)
+  updateHref()
+}
+
+history.replaceState = function() {
+  originalReplaceState.apply(this, arguments)
+  updateHref()
+}
+
+window.addEventListener('popstate', updateHref)
+window.addEventListener('hashchange', updateHref)
+
+export const url = derived(href, $href => new URL($href))
