298c38e94b8812b6d1d07ef3ebd775921e316af0
brett <brettinternet@gmail.com>
2021-07-11 20:52:12 -0600

    feat: add date i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,10 +2,21 @@
  * @docs https://github.com/kaisermann/svelte-i18n/blob/main/docs/Getting%20Started.md
  */
 import { register, init, getLocaleFromNavigator } from 'svelte-i18n'
+import en from 'date-fns/locale/en-US'
 
-register('en', () => import('./en.json'))
+export type Locales = 'en'
 
-init({
-  fallbackLocale: 'en',
-  initialLocale: getLocaleFromNavigator(),
-})
+export const setup = () => {
+  register('en', () => import('./en.json'))
+
+  init({
+    fallbackLocale: 'en',
+    initialLocale: getLocaleFromNavigator(),
+  })
+}
+
+export const getDateLocale = (locale: Locales | string) => {
+  if (locale.includes('en')) {
+    return en
+  }
+}
