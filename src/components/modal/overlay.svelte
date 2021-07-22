ed4263d7092c77e95fdf7001336f3287de068329
brett <brettinternet@gmail.com>
2021-07-22 15:37:00 -0600

    refactor: use native transition for overlay fade

--- src/components/modal/overlay.svelte
+++ src/components/modal/overlay.svelte
@@ -1,17 +1,8 @@
 <script lang="ts">
-  import cn from 'classnames'
-  import { onMount } from 'svelte'
-
-  let mounted = false
-
-  onMount(() => {
-    mounted = true
-  })
+  import { fade } from 'svelte/transition'
 </script>
 
 <div
-  class={cn(
-    'hidden lg:block fixed z-overlay top-0 right-0 bottom-0 left-0 bg-gray-800 transition-all',
-    mounted ? 'bg-opacity-40' : 'bg-opacity-0'
-  )}
+  transition:fade={{ duration: 150 }}
+  class="hidden lg:block fixed z-overlay top-0 right-0 bottom-0 left-0 bg-gray-800 transition-all bg-opacity-30"
 />
