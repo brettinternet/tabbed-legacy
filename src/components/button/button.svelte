30fd9b6d5a3421fa6cc755dffaedd9a14c9bf6a2
brett <brettinternet@gmail.com>
2021-07-29 00:27:39 -0600

    feat: create secondary styles for button

--- src/components/button/button.svelte
+++ src/components/button/button.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
+  import cn from 'classnames'
+
   export let onClick: OptionalProp<
-    svelte.JSX.MouseEventHandler<HTMLButtonElement>
-  > = undefined
+      svelte.JSX.MouseEventHandler<HTMLButtonElement>
+    > = undefined,
+    secondary: OptionalProp<boolean> = undefined
 </script>
 
 <button
   on:click={onClick}
-  class="py-1 px-2 rounded-md bg-blue-600 text-white dark:text-black dark:bg-blue-300"
+  class={cn(
+    'py-1 px-2 rounded-md',
+    secondary
+      ? 'bg-gray-200 text-black dark:bg-gray-700 dark:text-gray-100'
+      : 'bg-blue-600 text-white dark:bg-blue-300 dark:text-black'
+  )}
   {...$$restProps}
 >
   <slot />
