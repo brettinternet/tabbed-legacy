3f01163e38b59e9336fd4ac819343d0933f5f050
brett <brettinternet@gmail.com>
2021-08-09 23:14:26 -0600

    feat: clear search button

--- src/components/search/search.svelte
+++ src/components/search/search.svelte
@@ -5,6 +5,7 @@
   import Input from 'src/components/input/input.svelte'
   import { searchValue } from 'src/components/search/store'
   import { searchSessions } from 'src/components/search/send'
+  import X from 'src/components/icons/x.svelte'
 
   const submitSearch = async (text?: string) => {
     searchValue.set(text)
@@ -30,16 +31,37 @@
     void submitSearch(query)
   }
 
+  const clear: svelte.JSX.MouseEventHandler<HTMLButtonElement> = () => {
+    searchValue.set(undefined)
+    if (input) {
+      input.value = ''
+      input.focus()
+    }
+  }
+
   const debouncedChange = debounce(handleChange, 250)
 </script>
 
-<form role="search" on:submit={handleSubmit} aria-label="Sessions and tabs">
+<form
+  role="search"
+  on:submit={handleSubmit}
+  aria-label="Sessions and tabs"
+  class="relative"
+>
   <Input
     id="search"
-    classNames="w-full"
+    classNames="w-full xxs:pr-8"
     type="text"
     placeholder="Search"
     onInput={debouncedChange}
     bind:ref={input}
   />
+  {#if $searchValue}
+    <button
+      class="hidden xxs:flex items-center justify-center absolute top-0 right-0 h-full w-8"
+      on:click={clear}
+    >
+      <X />
+    </button>
+  {/if}
 </form>
